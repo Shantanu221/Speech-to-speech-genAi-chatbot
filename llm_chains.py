6511c33d0901024d902df4b40159e43f434c5eb5
Shantanu Ingale <128632085+Shantanu221@users.noreply.github.com>
2024-08-28 17:14:13 +0530

    llama 2 file

---
+++ llm_chains.py
@@ -0,0 +1,33 @@
+from prompt_templates import prompt_template
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.memory import ConversationBufferWindowMemory
+import yaml
+import os
+
+os.environ['HUGGINGFACEHUB_API_TOKEN']="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"
+
+with open("config.yaml","r") as f:
+    config=yaml.safe_load(f)
+
+def create_llm(model_path=config["model_path"]["chat_model_llama"],model_type=config["model_type"],model_config=config["model_config"]):
+    llm=CTransformers(model=model_path,model_type=model_type,config=model_config)
+    return llm
+
+def create_prompt_from_template(template):
+    return PromptTemplate(input_variables=["prompt"],template=template)
+
+def load_normal_chain():
+    return chatChain()
+
+class chatChain:
+
+    def __init__(self):
+        llm=create_llm()
+        chat_prompt=create_prompt_from_template(prompt_template)
+        # self.llm_chain=LLMChain(llm=llm,prompt=chat_prompt,memory=ConversationBufferWindowMemory(k=1))
+        self.llm_chain=LLMChain(llm=llm,prompt=chat_prompt)
+
+    def run(self,user_input):
+        return self.llm_chain.invoke(user_input)
